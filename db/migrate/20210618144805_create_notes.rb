ca5acdef8fbe59a330ff20d82651775ea7830e3b
Samantha Ostrowski <isphinxs@hotmail.com>
2021-06-18 13:06:01 -0400

    Switch database to postgresql

--- db/migrate/20210618144805_create_notes.rb
+++ db/migrate/20210618144805_create_notes.rb
@@ -2,7 +2,7 @@ class CreateNotes < ActiveRecord::Migration[6.1]
   def change
     create_table :notes do |t|
       t.string :content
-      t.boolean :is_active
+      t.boolean :is_active, default: true
       t.references :calendar, null: false, foreign_key: true
       t.references :day, null: false, foreign_key: true
 
